54387bedb4b01b4dbe326721c5c7e90484b94d5c
Ahmed Elkady <ahmed.elkady757@gmail.com>
2024-04-15 23:52:40 +0200

    New Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,11 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:stylish/view/forgot_password.dart';
-import 'package:stylish/view/getstarted.dart';
-import 'package:stylish/view/onboard_page1.dart';
-import 'package:stylish/view/signin.dart';
-import 'package:stylish/view/signup.dart';
-import 'package:stylish/view/splash.dart';
+import 'package:stylish/core/utils/navigation.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -24,24 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        Splash.id: (context) => const Splash(),
-        Signin.id: (context) => const Signin(),
-        Signup.id: (context) => const Signup(),
-        ForgotPassword.id: (context) => const ForgotPassword(),
-        GetStarted.id: (context) => const GetStarted(),
-        OnboardPage1.id1: (context) => const OnboardPage1(
-              index: 0,
-            ),
-        OnboardPage1.id2: (context) => const OnboardPage1(
-              index: 1,
-            ),
-        OnboardPage1.id3: (context) => const OnboardPage1(
-              index: 2,
-            ),
-      },
       debugShowCheckedModeBanner: false,
-      initialRoute:Splash.id,
+      onGenerateRoute: Navigation.onGenerateRoute,
+      onGenerateInitialRoutes:(_)=>Navigation.routes,
     );
   }
 }
+
