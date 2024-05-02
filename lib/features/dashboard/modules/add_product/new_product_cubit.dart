3483f8d8c48c382968ac209aa9b0d6ab5e4e7cf0
AdhamKhaled74 <kadham67153@gmail.com>
2024-05-02 15:14:15 +0300

    //AdhamNOTIFICATION

---
+++ lib/features/dashboard/modules/add_product/new_product_cubit.dart
@@ -0,0 +1,35 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:meta/meta.dart';
+
+import '../users/model/repo/database_users_data.dart';
+
+part 'new_product_state.dart';
+
+class NewProductCubit extends Cubit<NewProductState> {
+  NewProductCubit() : super(NewProductInitial());
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passcontroller = TextEditingController();
+  XFile? image;
+  Future<void> pickProductImage() async {
+    final ImagePicker picker = ImagePicker();
+// Pick an image.
+    image = await picker.pickImage(source: ImageSource.gallery);
+
+    emit(NewProductInitial());
+  }
+
+  Future<void> takeProductImage() async {
+    final ImagePicker picker = ImagePicker();
+// take an image.
+    image = await picker.pickImage(source: ImageSource.camera);
+    emit(NewProductInitial());
+  }
+
+  Future<void> addProduct(context) async {
+    DatabaseUsersData().insert(name: nameController.text, email: emailcontroller.text, password: passcontroller.text) ;
+  }
+}
---
+++ lib/features/dashboard/modules/add_product/new_product_state.dart
@@ -0,0 +1,6 @@
+part of 'new_product_cubit.dart';
+
+@immutable
+sealed class NewProductState {}
+
+final class NewProductInitial extends NewProductState {}
